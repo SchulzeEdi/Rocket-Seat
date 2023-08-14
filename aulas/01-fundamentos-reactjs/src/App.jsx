38121058ef00af1a378919b5829a51fc3d809780
Ederson Schulze <schulzeederson@gmail.com>
2023-08-14 13:24:20 -0300

    feito a sidebar e o header

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,18 +1,29 @@
 //JSX = JavaScript + XML (HTML)
 import { Post } from "./Post"
+import './global.css'
+import styles from './App.module.css'
+import { Header } from "./componentes/Header"
+import { Sidebar } from "./componentes/Sidebar"
 
 export function App() {
   return (
     <div>
-      <Post 
-        author= "Ederson Schulze" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sequi ipsam vitae necessitatibus quos corporis nobis similique eos minus architecto, deleniti sunt nulla soluta, mollitia minima cum rerum unde debitis?"
-      />
+      <Header/>
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post 
+            author= "Ederson Schulze" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sequi ipsam vitae necessitatibus quos corporis nobis similique eos minus architecto, deleniti sunt nulla soluta, mollitia minima cum rerum unde debitis?"
+          />
 
-      <Post
-        author="Guilherme Schulze"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sequi ipsam vitae necessitatibus quos corporis nobis similique eos minus architecto, deleniti sunt nulla soluta, mollitia minima cum rerum unde debitis?"
-      />
+          <Post
+            author="Guilherme Schulze"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sequi ipsam vitae necessitatibus quos corporis nobis similique eos minus architecto, deleniti sunt nulla soluta, mollitia minima cum rerum unde debitis?"
+          />
+        </main>
+      </div>
     </div>
   )
 }
